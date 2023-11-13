33c7fb10ccfa0af7cc0b97ef129a4115d5384fea
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-13 15:42:11 +0300

    Controller creation

---
+++ src/main/java/ru/padwicki/soundseeker/controllers/StSController.java
@@ -0,0 +1,9 @@
+package ru.padwicki.soundseeker.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.padwicki.soundseeker.controllersInterfaces.StSControllerInterface;
+
+@RequestMapping("/convert")
+public class StSController implements StSControllerInterface {
+
+}
---
+++ src/main/java/ru/padwicki/soundseeker/controllersInterfaces/StSControllerInterface.java
@@ -0,0 +1,8 @@
+package ru.padwicki.soundseeker.controllersInterfaces;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface StSControllerInterface {
+
+}

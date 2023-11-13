d7d223c79d83027f18a58eaa01a1ebbf71fb5493
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-13 19:47:41 +0300

    correcting

--- src/main/java/ru/padwicki/soundseeker/controllers/HubController.java
+++ src/main/java/ru/padwicki/soundseeker/controllers/HubController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.padwicki.soundseeker.config.authorization.SpotifyAuth;
+import ru.padwicki.soundseeker.config.auth.SpotifyAuth;
 import ru.padwicki.soundseeker.controllersInterfaces.HubControllerInterface;
 import ru.padwicki.soundseeker.service.HubService;
 

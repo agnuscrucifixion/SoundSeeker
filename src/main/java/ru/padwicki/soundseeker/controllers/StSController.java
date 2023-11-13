02443eef362da455c4301ab26e6c5eb67f2a14e2
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-13 23:40:38 +0300

    fixing annotation

--- src/main/java/ru/padwicki/soundseeker/controllers/StSController.java
+++ src/main/java/ru/padwicki/soundseeker/controllers/StSController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.padwicki.soundseeker.controllersInterfaces.StSControllerInterface;
+import ru.padwicki.soundseeker.pyBase.pyYandex;
 
-@Controller
+@RestController
 public class StSController implements StSControllerInterface {
 
     @Override
     public void Convert() {
-        // реализовывать, уже обращения к апи и тд
+
     }
 }

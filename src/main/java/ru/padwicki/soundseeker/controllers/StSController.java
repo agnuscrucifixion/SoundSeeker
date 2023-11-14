5d897de7669c7f77cfa92a94b68babe5838905a6
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-14 18:51:24 +0300

    Service-to-Service controller with Service-to-Service Service

--- src/main/java/ru/padwicki/soundseeker/controllers/StSController.java
+++ src/main/java/ru/padwicki/soundseeker/controllers/StSController.java
@@ -1,16 +1,25 @@
 package ru.padwicki.soundseeker.controllers;
 
+import org.apache.hc.core5.http.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.padwicki.soundseeker.controllersInterfaces.StSControllerInterface;
 import ru.padwicki.soundseeker.pyBase.pyYandex;
+import ru.padwicki.soundseeker.service.StsService;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+
+import java.io.IOException;
 
 @RestController
 public class StSController implements StSControllerInterface {
 
+    StsService stsService;
+    public StSController(StsService stsService) {
+        this.stsService = stsService;
+    }
     @Override
-    public void Convert() {
-
+    public void Convert(String id) throws IOException, ParseException, SpotifyWebApiException {
+        stsService.getTrack(id);
     }
 }

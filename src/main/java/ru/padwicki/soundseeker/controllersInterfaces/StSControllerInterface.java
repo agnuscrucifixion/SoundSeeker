918a6c2b927fe7478f824f730bdab2947b54351e
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-14 18:53:15 +0300

    correcting methods in S-to-S Interface

--- src/main/java/ru/padwicki/soundseeker/controllersInterfaces/StSControllerInterface.java
+++ src/main/java/ru/padwicki/soundseeker/controllersInterfaces/StSControllerInterface.java
@@ -1,10 +1,15 @@
 package ru.padwicki.soundseeker.controllersInterfaces;
 
+import org.apache.hc.core5.http.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+
+import java.io.IOException;
 
 @RequestMapping("/sts")
 public interface StSControllerInterface {
-    @GetMapping("/convert")
-    public void Convert();
+    @GetMapping("/convertTrack")
+    public void Convert(@RequestParam String id) throws IOException, ParseException, SpotifyWebApiException;
 }

d9dbede41bfacf930caebb9066de01d1bce75983
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-16 20:45:00 +0300

    initialize spotifyApi object and process with tokens and code

--- src/main/java/ru/padwicki/soundseeker/config/auth/SpotifyAuth.java
+++ src/main/java/ru/padwicki/soundseeker/config/auth/SpotifyAuth.java
@@ -1,4 +1,5 @@
 package ru.padwicki.soundseeker.config.auth;
+import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -7,16 +8,14 @@ import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.SpotifyHttpManager;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
-import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
-import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 
 import java.io.IOException;
 import java.net.URI;
-import java.text.ParseException;
+import java.net.URL;
 
-@Component
+@Component("SpotifyAuthURL")
 @PropertySource("classpath:application.properties")
 public class SpotifyAuth implements InitializingBean {
     @Value("${spotify.client_id}")
@@ -25,10 +24,26 @@ public class SpotifyAuth implements InitializingBean {
     private String clientSecret;
     private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/swagger-ui/index.html#/");
     private AuthorizationCodeUriRequest authorizationCodeUriRequest;
+    private AuthorizationCodeRequest authorizationCodeRequest;
+    private String code;
+    private URI url;
+    private SpotifyApi spotifyApi;
+    private String accessToken;
+    private String refreshToken;
 
+    public String[] clientCredentials(String codeLocal) throws IOException, ParseException, SpotifyWebApiException {
+        authorizationCodeRequest = spotifyApi.authorizationCode(codeLocal)
+                .build();
+        final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+        accessToken = authorizationCodeCredentials.getAccessToken();
+        spotifyApi.setAccessToken(accessToken);
+        refreshToken = authorizationCodeCredentials.getRefreshToken();
+        spotifyApi.setRefreshToken(refreshToken);
+        return new String[]{clientId, clientSecret, redirectUri.toString(), codeLocal, accessToken, refreshToken};
+    }
     @Override
     public void afterPropertiesSet() {
-       final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+        spotifyApi = new SpotifyApi.Builder()
                 .setClientId(clientId)
                 .setClientSecret(clientSecret)
                 .setRedirectUri(redirectUri)
@@ -37,6 +52,7 @@ public class SpotifyAuth implements InitializingBean {
                 .build();
     }
     public URI authorization() {
-        return authorizationCodeUriRequest.execute();
+        url = authorizationCodeUriRequest.execute();
+        return url;
     }
 }

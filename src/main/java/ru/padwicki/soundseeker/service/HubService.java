f524720ad6c450f999d5ba4a76e08775aed701b4
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-16 20:44:07 +0300

    correct hub service

--- src/main/java/ru/padwicki/soundseeker/service/HubService.java
+++ src/main/java/ru/padwicki/soundseeker/service/HubService.java
@@ -1,22 +1,27 @@
 package ru.padwicki.soundseeker.service;
 
+import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.padwicki.soundseeker.config.auth.SpotifyAuth;
 import ru.padwicki.soundseeker.serviceInterfaces.HubServiceInterface;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 
+import java.io.IOException;
 import java.net.URI;
 
 @Service
 public class HubService implements HubServiceInterface {
     SpotifyAuth spotifyAuth;
-
+    StsService stsService;
     @Autowired
-    public HubService(SpotifyAuth spotifyAuth) {
+    public HubService(SpotifyAuth spotifyAuth, StsService stsService) {
         this.spotifyAuth = spotifyAuth;
+        this.stsService = stsService;
     }
     @Override
-    public URI authorization() {
-        return spotifyAuth.authorization();
+    public URI authorization(){
+        URI url = spotifyAuth.authorization();
+        return url;
     }
 }
--- src/main/java/ru/padwicki/soundseeker/serviceInterfaces/HubServiceInterface.java
+++ src/main/java/ru/padwicki/soundseeker/serviceInterfaces/HubServiceInterface.java
@@ -1,7 +1,11 @@
 package ru.padwicki.soundseeker.serviceInterfaces;
 
+import org.apache.hc.core5.http.ParseException;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+
+import java.io.IOException;
 import java.net.URI;
 
 public interface HubServiceInterface {
-    public URI authorization();
+    public URI authorization() throws IOException, ParseException, SpotifyWebApiException;
 }

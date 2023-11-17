62824a58124f7123a01114c16639922ae7d6c110
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-17 22:21:51 +0300

    controller for start page and initialization start point of spotifyApi

--- src/main/java/ru/padwicki/soundseeker/controllers/HubController.java
+++ src/main/java/ru/padwicki/soundseeker/controllers/HubController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import ru.padwicki.soundseeker.controllersInterfaces.HubControllerInterface;
 import ru.padwicki.soundseeker.service.HubService;
+import ru.padwicki.soundseeker.service.StsService;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+
 import java.io.IOException;
 
 
@@ -13,14 +15,28 @@ import java.io.IOException;
 public class HubController implements HubControllerInterface {
 
     HubService hubService;
-
-    public HubController(HubService hubService) {
+    StsService stsService;
+    public HubController(HubService hubService, StsService stsService) {
         this.hubService = hubService;
+        this.stsService = stsService;
     }
 
     @Override
-    public String startPage(Model model) throws IOException, ParseException, SpotifyWebApiException {
-        model.addAttribute("urlSpotify", hubService.authorization());
+    public String startPage(Model model){
+        if (!model.containsAttribute("urlSpotify")) {
+            model.addAttribute("urlSpotify", hubService.authorization());
+        }
         return "hub";
+    }
+
+    @Override
+    public String handleCallBack(String code, Model model) throws IOException, ParseException, SpotifyWebApiException {
+        stsService.initApi(code);
+        return "service-to-service/sts";
+    }
+
+    @Override
+    public String convertPage() {
+        return "service-to-service/convert";
     }
 }

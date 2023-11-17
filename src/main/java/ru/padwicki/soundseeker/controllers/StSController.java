af32292639c398457aac83806c7a5be8c8631e48
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-17 22:25:39 +0300

    method(start point) for spotify-to-spotify

--- src/main/java/ru/padwicki/soundseeker/controllers/StSController.java
+++ src/main/java/ru/padwicki/soundseeker/controllers/StSController.java
@@ -2,40 +2,31 @@ package ru.padwicki.soundseeker.controllers;
 
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 import ru.padwicki.soundseeker.controllersInterfaces.StSControllerInterface;
-import ru.padwicki.soundseeker.pyBase.pyYandex;
 import ru.padwicki.soundseeker.service.StsService;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
-import springfox.documentation.annotations.ApiIgnore;
 
 import java.io.IOException;
 
-@RestController
+@Controller
 public class StSController implements StSControllerInterface {
 
     StsService stsService;
     public StSController(StsService stsService) {
         this.stsService = stsService;
     }
+
+
     @Override
-    public void convertTrack(String id) throws IOException, ParseException, SpotifyWebApiException {
-        stsService.getTrack(id);
+    public String spotifyToSpotify(Model model) throws IOException, ParseException, SpotifyWebApiException {
+        model.addAttribute("playlists", stsService.getOwnUserPlaylists());
+        return "service-to-service/spotify-to-spotify";
     }
 
     @Override
-    public void convertTrackFromAlbum(String id) throws IOException, ParseException, SpotifyWebApiException {
-        stsService.getTracksFromAlbum(id);
-    }
-
-    @Override
-    public void getOwnUserPlaylists() throws IOException, ParseException, SpotifyWebApiException {
-        stsService.getOwnUserPlaylists();
-    }
-
-    @Override
-    public void handleSwaggerRedirect(String code) throws IOException, ParseException, SpotifyWebApiException {
-        stsService.initApi(code);
+    public String show(String name, Model model) throws IOException, ParseException, SpotifyWebApiException {
+        model.addAttribute("playlist", stsService.show(name));
+        return "service-to-service/show";
     }
 }
--- src/main/java/ru/padwicki/soundseeker/controllersInterfaces/StSControllerInterface.java
+++ src/main/java/ru/padwicki/soundseeker/controllersInterfaces/StSControllerInterface.java
@@ -1,7 +1,9 @@
 package ru.padwicki.soundseeker.controllersInterfaces;
 
 import org.apache.hc.core5.http.ParseException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
@@ -11,15 +13,13 @@ import java.io.IOException;
 
 @RequestMapping("/sts")
 public interface StSControllerInterface {
-    @GetMapping("/convertTrack")
-    void convertTrack(@RequestParam String id) throws IOException, ParseException, SpotifyWebApiException;
 
-    @GetMapping("/convertTrackFromAlbum")
-    void convertTrackFromAlbum(@RequestParam String id) throws IOException, ParseException, SpotifyWebApiException;
+    @GetMapping("/spotify-to-spotify")
+    String spotifyToSpotify(Model model) throws IOException, ParseException, SpotifyWebApiException;
 
-    @GetMapping("/getOwnUserPlaylists")
-    void getOwnUserPlaylists() throws IOException, ParseException, SpotifyWebApiException;
 
-    @GetMapping("/handle")
-    void handleSwaggerRedirect(@RequestParam("code") String code) throws IOException, ParseException, SpotifyWebApiException;
+
+
+    @GetMapping("/{name}")
+    String show(@PathVariable("name") String name, Model model) throws IOException, ParseException, SpotifyWebApiException;
 }

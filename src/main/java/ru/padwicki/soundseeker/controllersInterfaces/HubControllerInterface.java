364347ad73b745c3d154d6c64ac43dc2f0ae2225
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-17 22:22:05 +0300

    controllerInterface for start page and initialization start point of spotifyApi

--- src/main/java/ru/padwicki/soundseeker/controllersInterfaces/HubControllerInterface.java
+++ src/main/java/ru/padwicki/soundseeker/controllersInterfaces/HubControllerInterface.java
@@ -4,6 +4,7 @@ import org.apache.hc.core5.http.ParseException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 
@@ -13,4 +14,9 @@ public interface HubControllerInterface {
     @GetMapping("/hub")
     String startPage(Model model) throws IOException, ParseException, SpotifyWebApiException;
 
+    @GetMapping("/callback")
+    String handleCallBack(@RequestParam("code") String code, Model model) throws IOException, ParseException, SpotifyWebApiException;
+
+    @GetMapping("/convert")
+    String convertPage();
 }
